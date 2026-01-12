56d30915c2b4a9e03d39263ab7d283df7310d429
kumardeepakg92 <kumardeepakg92@gmail.com>
2026-01-13 00:53:02 +0530

    build completed

--- src/recommender.py
+++ src/recommender.py
@@ -1,4 +1,5 @@
 from langchain.chains import RetrievalQA
+#from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain_groq import ChatGroq
 from src.prompt_template import get_anime_prompt
 
