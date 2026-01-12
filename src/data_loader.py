81ede39a8e41d5acf7c57ebdfddefbf08b7c4b34
kumardeepakg92 <kumardeepakg92@gmail.com>
2026-01-12 10:54:30 +0530

    prompt_template

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,19 +1,23 @@
 import pandas as pd
 
 class AnimeDataLoader:
-    def __init__(self, original_csv:str,processed_csv:str):
+    def __init__(self,original_csv:str,processed_csv:str):
         self.original_csv = original_csv
         self.processed_csv = processed_csv
 
     def load_and_process(self):
-        # Load the original CSV file
-        df = pd.read_csv(self.original_csv)
+        df = pd.read_csv(self.original_csv , encoding='utf-8' , on_bad_lines='skip').dropna()
 
-        # Basic processing: drop duplicates and handle missing values
-        df.drop_duplicates(inplace=True)
-        df.fillna("Unknown", inplace=True)
+        required_cols = {'Name' , 'Genres','sypnopsis'}
 
-        # Save the processed data to a new CSV file
-        df.to_csv(self.processed_csv, index=False)
+        missing = required_cols - set(df.columns)
+        if missing:
+            raise ValueError("Missing column  in CSV File")
+        
+        df['combined_info'] = (
+            "Title: " + df["Name"] + " Overview: " +df["sypnopsis"] + "Genres : " + df["Genres"]
+        )
 
-        return df
+        df[['combined_info']].to_csv(self.processed_csv , index=False,encoding='utf-8')
+
+        return self.processed_csv
